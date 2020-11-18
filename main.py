3a7f8c90c0a8722d5692f60e615128c3e225ad06
jcoumont <jcoumont@gmail.com>
2020-11-18 21:50:00 +0100

    scrapping the javascript to create dico to read desired data

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ if __name__ == "__main__":
     my_immoweb_api = ImmowebAPI()
     # Get list on 1st Page
     list_url = my_immoweb_api.get_properties_list()
-    print(len(list_url))
-    # Get list on 54th Page
-    list_url54 = my_immoweb_api.get_properties_list(54)
-    print(len(list_url54))
+    for annonce_url in list_url:
+        my_immoweb_api.get_properties_detail(annonce_url)
--- src/immoweb_api.py
+++ src/immoweb_api.py
@@ -3,6 +3,7 @@ import re
 import bs4
 import requests
 import selenium
+import demjson
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -29,7 +30,7 @@ class ImmowebAPI():
         soup = BeautifulSoup(driver.page_source, 'lxml')
         for a in soup.find_all('a', attrs={"class": "card__title-link"}):
             print(a['href'])
-            properties_list.append(a)
+            properties_list.append(a['href'])
         driver.close()
         return properties_list
 
@@ -41,4 +42,21 @@ class ImmowebAPI():
         :param annonce_url : url to call
         :return : an object that represent the detail
         '''
-        pass
+        driver = webdriver.Firefox()
+        driver.get(annonce_url)
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+
+        script = soup.find_all('script')[1].contents[0]
+
+        # Step 1: remove all spaces, new lines and tabs
+        script = (re.sub('[\s+;]', '', script))
+
+        # Step 2: convert string to dictionary. Creates a key as "digitalData"
+        js_dict = script.split("=")[1]
+        js_dict = re.sub(r"\bwindow(.*?),\b", '"",', js_dict)
+        my_dict = demjson.decode(js_dict)
+
+        # print(my_dict)
+        print((my_dict['classified'])['id']) # 9034494
+ type(my_dict[0]['classified']['id'])
+        driver.close()
