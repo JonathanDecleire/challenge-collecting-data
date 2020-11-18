ff5babc48ae99d5916f8a7d0ab12588bb3d98299
jcoumont <jcoumont@gmail.com>
2020-11-18 15:10:05 +0100

    Adding return in immowebAPI get_properties_list with only URL and call it from the main.py

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from src.immoweb_api import ImmowebAPI
 
 if __name__ == "__main__":
     my_immoweb_api = ImmowebAPI()
-    my_immoweb_api.get_properties_list()
+    # Get list on 1st Page
+    list_url = my_immoweb_api.get_properties_list()
+    print(len(list_url))
+    # Get list on 54th Page
+    list_url54 = my_immoweb_api.get_properties_list(54)
+    print(len(list_url54))
--- src/immoweb_api.py
+++ src/immoweb_api.py
@@ -7,16 +7,31 @@ import selenium
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from typing import List
 
 
 class ImmowebAPI():
     def __init__(self):
-        self.base_url = 'https://www.immoweb.be/fr/recherche/maison-et-appartement/a-vendre?countries=BE&page=###&orderBy=relevance'
+        self.base_search_url = 'https://www.immoweb.be/fr/recherche/maison-et-appartement/a-vendre?countries=BE&page={}&orderBy=relevance'
         pass
 
-    def get_properties_list(self):
+    def get_properties_list(self, page_num: int = 1) -> List[str]:
+        '''
+        This function will return the list of properties url found
+        on a immoweb search page
+
+        :param page_num : int that represent the page number to inspect
+        :return : a list of url founded in class "card__title-link"
+        '''
+        properties_list = []
         driver = webdriver.Firefox()
-        driver.get(self.base_url.replace('##', '1'))
-        soup = BeautifulSoup(driver.page_source)
-        for lien in soup.find_all('a', attrs={"class": "card__title-link"}):
-            print(lien)
+        driver.get(self.base_search_url.format(page_num))
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        for a in soup.find_all('a', attrs={"class": "card__title-link"}):
+            print(a['href'])
+            properties_list.append(a)
+        driver.close()
+        return properties_list
+    
+    def get_properties_detail(self):
+        pass
