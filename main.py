cfb0806b5f70a0921451474bc34c321311a8531d
jcoumont <jcoumont@gmail.com>
2020-11-18 23:28:34 +0100

    First attempt to fill an instance of PropertyDetail

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ if __name__ == "__main__":
     # Get list on 1st Page
     list_url = my_immoweb_api.get_properties_list()
     for annonce_url in list_url:
-        my_immoweb_api.get_properties_detail(annonce_url)
+        try:
+            my_immoweb_api.get_properties_detail(annonce_url)
+        except Exception as e:
+            print(f'unable to retrieve info from this URL : {annonce_url}\nError message : {e}')
