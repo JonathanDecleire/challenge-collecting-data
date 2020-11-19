b0663b27062986d3b0611dd17313c0ab5539a19a
jcoumont <jcoumont@gmail.com>
2020-11-19 08:44:56 +0100

    adding database file

---
+++ src/database.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from src.property_detail import PropertyDetail
+
+
+class Database():
+    '''
+    Class ...
+    '''
+    def __init__(self, database_file: str):
+        pass
+
+    def add_property_detail(self, property_detail: PropertyDetail):
+        pass
+
+    def load_database(self) -> List[PropertyDetail]:
+        pass
